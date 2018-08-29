cb84527ddd117d794ef2b9933e0147eca8057c8a
nanasemaik <32819413+nanasemaik@users.noreply.github.com>
2018-08-29 10:53:53 +0800

    Update fix字幕侠.py
    fix字幕组 爬取

--- fix字幕侠.py
+++ fix字幕侠.py
@@ -1,8 +1,3 @@
-# 作者：知乎用户
-# 链接：https://www.zhihu.com/question/28661987/answer/409020894
-# 来源：知乎
-# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-
 # -*- coding:utf-8 -*-
 
 import requests
@@ -15,29 +10,7 @@ emule = []
 
 fix_url = input('请输入剧集地址\n格式为: http://www.zimuxia.cn/portfolio/[剧集名]\n')
 #fix_url = 'http://www.zimuxia.cn/portfolio/%e6%9c%89%e5%ae%b6%e5%8f%af%e5%bd%92%e7%9a%84%e6%81%8b%e4%ba%ba%e4%bb%ac'
-# with requests.Session() as session:
-    # resp = session.get(tv_url, timeout=10)
-    # resp.raise_for_status()
-    # soup = BeautifulSoup(resp.content, 'lxml', from_encoding=resp.encoding)
-
-    # baidu_text = soup.find_all(text='百度网盘')
-    # if baidu_text != None:
-        # for i in range(len(baidu_text)):
-            # baidu_parent = baidu_text[i].parent
-            # baidu.append(baidu_parent.get('href'))
-
-    # magnet_text = soup.find_all(text='磁力下载')
-    # if magnet_text != None:
-        # for j in range(len(magnet_text)):
-            # magnet_parent = magnet_text[j].parent
-            # magnet.append(magnet_parent.get('href'))
-
-    # emule_text = soup.find_all(text='电驴下载')
-    # if emule_text != None:
-        # for k in range(len(emule_text)):
-            # emule_parent = emule_text[k].parent
-            # emule.append(emule_parent.get('href'))
-            
+       
 r = requests.get(fix_url, headers={'content-type': 'text/html; charset=UTF-8', 'User-Agent': 'Mozilla/5.0'})
 soup = BeautifulSoup(r.text, 'html.parser', from_encoding='utf-8')
 
